9664e8a0cc50f4f6c9b025285cf4ed832602c11f
twidi <s.angel@twidi.com>
2015-06-22 19:44:43 +0200

    Make run_tests.py more usable
    It's now possible to easily pass path to specific directories or python
    files in the dataql package

--- run_tests.py
+++ run_tests.py
@@ -1,114 +1,108 @@
 #!/usr/bin/env python
 """
-(https://gist.github.com/ishikawa/559312)
+(First inspired by https://gist.github.com/ishikawa/559312)
 
 The **run_tests.py** script supports simple test discovery (both `doctest`_ and `unittest`_)
 and running discovered tests.
 
- * ``-s DIRECTORY``  Directory to start discovery ('.' default)
- * ``-t DIRECTORY``  Top level directory of project (default to start directory)
-
-How to use::
-
-    % PYTHONPATH=.:project_dir python tests/runtests.py -s . -t project_dir
+Run `./run_tests.py to see usage.
 
 .. _doctest:  http://docs.python.org/library/doctest.html
 .. _unittest: http://docs.python.org/library/unittest.html
 
 """
  
-import sys
+import argparse
+from importlib import import_module
 import os
-from os.path import exists, join
- 
+
 import unittest
 import doctest
  
- 
-def walk_modules(root_dir):
-    """Generate the modules in a directory tree by walking the tree."""
- 
-    for root, dirs, files in os.walk(root_dir):
-        dirs[:] = [d for d in dirs
-                           if not d.startswith(".") and
-                              exists(join(root, d, "__init__.py"))]
-        files[:] = ["" if f.startswith("__init__") else f[:-3]
-                            for f in files
-                                  if f.endswith(".py")]
- 
-        root = relpath(root, root_dir)
- 
-        if not root or root == ".":
-            continue
- 
-        root = root.split("/")
-        modules = [root + [f] if f else root for f in files]
- 
-        for name in [".".join(m) for m in modules]:
-            __import__(name)
-            yield sys.modules[name]
- 
-def relpath(path, start):
-    if hasattr(os.path, "relpath"):
-        return os.path.relpath(path, start)
-    else:
-        # os.path.relpath is new in 2.6
-        import re
-        from os.path import normpath
- 
-        pattern = normpath(start)
-        pattern = re.compile(r'^%s/?' % re.escape(pattern))
-        return pattern.sub('', normpath(path))
- 
-def discover(start_directory, top_directory=None):
- 
-    if not top_directory:
-        top_directory = start_directory
- 
+
+package_name = 'dataql'
+top_dir = os.path.abspath(os.path.dirname(__file__))
+package_dir = os.path.join(top_dir, package_name)
+
+
+def get_modules(paths):
+    files = [p for p in paths if p.endswith('.py')]
+
+    for dir in [p for p in paths if not p.endswith('.py')]:
+        for root_dir, _, sub_files in os.walk(dir):
+            files.extend([
+                os.path.join(root_dir, f) for f in sub_files if f.endswith('.py')
+            ])
+
+    modules = []
+    for f in sorted(files):
+        module_path = os.path.relpath(f, top_dir).replace('/', '.')[:-3].replace('.__init__', '')
+        modules.append(import_module(module_path))
+
+    return modules
+
+
+def discover(paths):
+
     loader = unittest.defaultTestLoader
-    suite  = unittest.TestSuite()
- 
+    suite = unittest.TestSuite()
+
+    modules = get_modules(paths)
+
     # doctest
-    for module in walk_modules(top_directory):
+    for module in modules:
         try:
             tests = doctest.DocTestSuite(module)
         except ValueError:
             pass
         else:
             suite.addTest(tests)
- 
+
     # unittest
-    for module in walk_modules(start_directory):
+    for module in modules:
         name = module.__name__.split(".")[-1]
         if not name.startswith("test_"):
             continue
- 
+
         tests = loader.loadTestsFromModule(module)
         suite.addTest(tests)
- 
+
     return suite
- 
+
+
+def project_path(path):
+    if not path.startswith('/'):
+        path = os.path.join(top_dir, path)
+    path = os.path.normpath(path)
+
+    if package_dir not in path:
+        raise argparse.ArgumentTypeError('Path should be "%s" or subdirectory '
+                                         'or python file in this tree' % package_dir)
+
+    if not os.path.exists(path):
+        raise argparse.ArgumentTypeError('Path "%s" does not exist' % package_dir)
+
+    if path.endswith('.py') and not os.path.isfile(path):
+        raise argparse.ArgumentTypeError('Path ending with .py must be a file')
+
+    if not path.endswith('.py') and not os.path.isdir(path):
+        raise argparse.ArgumentTypeError('Path must be a directory')
+
+    return path
+
+
 def main():
-    from optparse import OptionParser
- 
-    parser = OptionParser()
-    parser.add_option("-s", "--start",
-                      dest="start", default=".",
-                      help="Directory DIRECTORY to start discovery ('.' default)",
-                      metavar="DIRECTORY")
-    parser.add_option("-t", "--top",
-                      dest="top",
-                      help="Top level directory of project (default to start directory)",
-                      metavar="DIRECTORY")
- 
-    (options, args) = parser.parse_args()
- 
+    parser = argparse.ArgumentParser(description='Discover and run tests.')
+    parser.add_argument('paths', metavar='PATH', type=project_path, nargs='*',
+                        default=[package_dir],
+                        help='One or more paths (directory or python file) for which tests '
+                             'should be run. Default to "dataql", to tests the whole package')
+
+    args = parser.parse_args()
+
     runner = unittest.TextTestRunner()
-    runner.run(discover(
-                start_directory=options.start,
-                top_directory=options.top))
- 
- 
+    runner.run(discover(paths=args.paths))
+
+
 if __name__ == "__main__":
     main()
-
